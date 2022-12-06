46a60be92f2c696d0ad9b46617c0512c22783f55
Maxim <zareff@yandex.ru>
2022-12-06 22:08:16 +0300

    Seminar03 Home Tasks  17, 18, 20, 22 added

---
+++ Seminar03Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int quarter = ReadData("Введите четверть: ");
+
+PrintAnswer(quarter);
+
+// Метод нхождения значений X и Y по четверти
+
+void PrintAnswer(int num)
+{
+    if (( num > 0) && (num < 5))
+    {
+        if ( num == 1) Console.WriteLine("X > 0, Y > 0");
+        if ( num == 2) Console.WriteLine("X > 0, Y < 0");
+        if ( num == 3) Console.WriteLine("X < 0, Y < 0");
+        if ( num == 4) Console.WriteLine("X < 0, Y > 0");
+    }
+    else Console.WriteLine("Вы ввели неверный номер четверти!");
+}
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
---
+++ Seminar03Task20/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №20 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int coordX01 = ReadData("Введите координату X первой точки: ");
+int coordY01 = ReadData("Введите координату Y первой точки: ");
+
+int coordX02 = ReadData("Введите координату X второй точки: ");
+int coordY02 = ReadData("Введите координату Y второй точки: ");
+
+double length = CalculateLength( coordX01, coordY01, coordX02, coordY02);
+PrintData("Расстояние между точками X и Y равна ", length);
+
+// Метод вывода данных
+
+void PrintData( string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Метод нахождения расстояния L= Sqrt((x01-x02)*(x01-x02)+ (y01-y02)*(y01-y02))
+
+double CalculateLength( double X01, double Y01, double X02, double Y02)
+{
+    double leng = Math.Sqrt(Math.Pow((X01-X02),2)+ Math.Pow((Y01-Y02),2));
+    return leng;
+}
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
