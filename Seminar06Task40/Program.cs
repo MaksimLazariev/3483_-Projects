c44d475c8b1eb29dd06b9f0891e77560dfb7256b
Maxim <zareff@yandex.ru>
2022-12-20 19:54:14 +0300

    Seminar05 Tasks 39, 40, 42 added

---
+++ Seminar06Task40/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 40  Вводим 3 числа. Нужно определить, может ли существовать тругольник с такими стронами
+
+// int a = ReadData("Введите первое число: ");
+// int b = ReadData("Введите второе число: ");
+// int c = ReadData("Введите третье число: ");
+
+// if (TriangleTest(a, b, c)) PrintData("Может существовать треугольник сo сторонами такой длины.");
+// else PrintData("Треугольник сo сторонами такой длины не существует.");
+// //Метод, считывающий данные, введенные пользователем
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+// //Метод, выводящий на печать данные пользователю
+// void PrintData(string res)
+// {
+//     Console.WriteLine(res);
+// }
+
+// bool TriangleTest(int a, int b, int c)
+// {
+//     return ((a + b > c) && (a + c > b) && (b + c > a));
+// }
+int A = ReadData("Enter first number: ");//1c
+int B = ReadData("Enter second number: ");//1c
+int C = ReadData("Enter third number: ");//1c
+//TriangleTest(A,B,C);//50c
+PrintData ("Result: "  + TriangleTest(A,B,C));//1
+
+//103c
+
+// Метод ввода
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Тест на треугольник
+bool TriangleTest (int a, int b, int c)
+{
+    return ((a+b>c)&& (a+c>b)&& (b+c>a));
+}
---
+++ Seminar06Task42/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 42 Программа преобразовывает 10ные числа в 2ные
+
+// Вводим число
+int inputNumber = ReadData("Enter number: ");
+
+PrintData("Result: " + DegToBin(inputNumber));              
+PrintData("Result: " + DecToBinNativ(inputNumber));            // Переводим в 2ную систему
+PrintData("Result: " + Convert.ToString(inputNumber, 8));      // Переводим в 8ную систему 
+PrintData("Result: " + Convert.ToString(inputNumber, 16));     // Переводим в 16ную систему
+
+// Метод ввода
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод перевода из 10ного числа в 2ное число  
+string DegToBin(int num)
+{
+    string outBin = String.Empty;
+    while (num > 0)
+    {
+        outBin = (num % 2) + outBin;
+        num = num / 2;
+    }
+    return outBin;
+}
+// Сстемный метод конвертации
+string DecToBinNativ(int num)
+{
+    return Convert.ToString(num, 2);
+}
