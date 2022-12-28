4e15e8cd958e4b9c89bd4315c8af645bfb7ba1c8
Maxim <zareff@yandex.ru>
2022-12-29 00:58:59 +0300

    Seminar09 Home Tasks 64, 66, 68 added

---
+++ Seminar09Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 66
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix.ToString());
+}
+
+// Нахождение суммы от M до N рекурентно 
+int RecSumMN(int M, int N)
+{
+    int sum=0;
+    if (M >= N) return sum +=N;             // точка остановки
+     sum += M +  RecSumMN(M + 1, N);        // изменение при вызове
+    return sum;
+}
+
+// ---------------- Тело программы --------------------------
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+int sumMN= number1<number2? RecSumMN(number1, number2):RecSumMN(number2, number1);
+PrintResult(sumMN);
---
+++ Seminar09Task68/Program.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 68
+//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// ---------------------------------------------------------------------
+//  m = 2, n = 3 -> A(m,n) = 9
+//  m = 3, n = 2 -> A(m,n) = 29
+//
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// Нахождение суммы от M до N рекурентно 
+int AccermanFunc(int N, int M)
+{
+    if (N==0) return M+1;                                  // точка остановки
+    if (M==0 && N!=0) return AccermanFunc(N-1, 1);          
+    return AccermanFunc(N-1, AccermanFunc(N , M-1) ) ;     // изменение при вызове
+}
+
+// ---------------- Тело программы --------------------------
+
+int numberN = ReadData("Введите число n: ");
+int numberM = ReadData("Введите число m: ");
+// проверяем, что введены положительные числа 
+if(numberN>=0 && numberM>=0)
+{   
+    int accerman = AccermanFunc( numberN, numberM);
+    PrintResult($"Функция Аккермана A({numberN},{numberM}) равна {accerman}");
+}
+else PrintResult("Ошибка! Введите числа n и m больше или равные 0"); 
